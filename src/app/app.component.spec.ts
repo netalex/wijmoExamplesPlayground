10ed86f551cf0c074391eca12a065e968752ccd9
Alessandro Aprile <aprile.alessandro@gmail.com>
2020-02-11 15:15:43 +0100

    generated test with simonTest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,21 @@
-import {async, TestBed} from '@angular/core/testing';
-import {RouterTestingModule} from '@angular/router/testing';
-import {AppComponent} from './app.component';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { AppComponent } from './app.component';
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+      schemas: [NO_ERRORS_SCHEMA],
+      declarations: [AppComponent]
+    });
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
-
-  it(`should have as title 'wijmoExamplesPlayground'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('wijmoExamplesPlayground');
+  it('can load instance', () => {
+    expect(component).toBeTruthy();
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('wijmoExamplesPlayground app is running!');
+  it('title defaults to: wijmoExamplesPlayground', () => {
+    expect(component.title).toEqual('wijmoExamplesPlayground');
   });
 });
--- src/app/example-from-blog/example-from-blog.component.spec.ts
+++ src/app/example-from-blog/example-from-blog.component.spec.ts
@@ -1,25 +1,21 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-
-import {ExampleFromBlogComponent} from './example-from-blog.component';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ExampleFromBlogComponent } from './example-from-blog.component';
 describe('ExampleFromBlogComponent', () => {
   let component: ExampleFromBlogComponent;
   let fixture: ComponentFixture<ExampleFromBlogComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ExampleFromBlogComponent]
-    })
-      .compileComponents();
-  }));
-
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      schemas: [NO_ERRORS_SCHEMA],
+      declarations: [ExampleFromBlogComponent]
+    });
     fixture = TestBed.createComponent(ExampleFromBlogComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
-
-  it('should create', () => {
+  it('can load instance', () => {
     expect(component).toBeTruthy();
+  });
+  it('title defaults to: Wijmo Starter App', () => {
+    expect(component.title).toEqual('Wijmo Starter App');
   });
 });
